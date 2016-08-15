09d583b805b1fec26ef06c568ce74db468ad2b81
wambo <allmaechtigerzerstoerer@gmail.com>
2016-08-15 23:37:01 +0200

    Logn

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PolymerElement } from '@vaadin/angular2-polymer';
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-list',
   templateUrl: 'list.component.html',
-  styleUrls: ['list.component.css']
+  styleUrls: ['list.component.css'],
+  directives: [
+    PolymerElement('paper-card'),
+    PolymerElement('paper-checkbox'),
+    PolymerElement('paper-fab'),
+    PolymerElement('google-map')
+  ]
+
 })
 export class ListComponent implements OnInit {
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PolymerElement} from '@vaadin/angular2-polymer';
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-login',
   templateUrl: 'login.component.html',
-  styleUrls: ['login.component.css']
+  styleUrls: ['login.component.css'],
+  directives: [
+    PolymerElement('paper-input'),
+    PolymerElement('paper-card'),
+    PolymerElement('paper-button'),
+    PolymerElement('iron-icon')
+  ]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {
   }
